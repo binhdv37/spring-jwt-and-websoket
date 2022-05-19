ef7477197423cfa99321c38563e2f5ed2cd813d7
Doan Van Binh <vanbinhdoan971@gmail.com>
2022-05-19 20:57:45 +0700

    add crud user controller

--- src/main/java/com/example/jwtandwebsocket/controller/UserController.java
+++ src/main/java/com/example/jwtandwebsocket/controller/UserController.java
@@ -2,12 +2,11 @@ package com.example.jwtandwebsocket.controller;
 
 import com.example.jwtandwebsocket.common.constant.AuthorityConstant;
 import com.example.jwtandwebsocket.common.exception.MyAppException;
+import com.example.jwtandwebsocket.dto.user.UserDto;
+import com.example.jwtandwebsocket.service.security.model.SecurityUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,5 +18,24 @@ public class UserController extends BaseController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getUserById(@PathVariable("id") UUID id) throws MyAppException {
         return ResponseEntity.ok(checkNullAndToBaseResp(userService.findById(id)));
+    }
+
+    @PreAuthorize("hasAnyAuthority(\"" + AuthorityConstant.USER_VIEW + "\")")
+    @GetMapping(value = "/all")
+    public ResponseEntity<?> getAllUser() throws MyAppException {
+        return ResponseEntity.ok(checkNullAndToBaseResp(userService.findAll()));
+    }
+
+    @PreAuthorize("hasAnyAuthority(\"" + AuthorityConstant.USER_CREATE + "\", \"" + AuthorityConstant.USER_CREATE + "\")")
+    @PostMapping(value = "")
+    public ResponseEntity<?> saveUser(@RequestBody UserDto userDto) throws MyAppException {
+        SecurityUser securityUser = getCurrentUser();
+        return ResponseEntity.ok(checkNullAndToBaseResp(userService.save(userDto, securityUser.getId())));
+    }
+
+    @PreAuthorize("hasAnyAuthority(\"" + AuthorityConstant.USER_DELETE + "\")")
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) throws MyAppException {
+        return ResponseEntity.ok(userService.deleteById(id));
     }
 }
